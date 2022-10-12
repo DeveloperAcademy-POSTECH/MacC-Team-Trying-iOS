ee349ac17a69e9e727b8d324a59889cc1f267849
kimscastle <uisong26559287@gmail.com>
2022-10-12 15:06:13 +0900

    [문서] 프로젝트생성

---
+++ Sources/Presenter/Home/HomeViewController.swift
@@ -0,0 +1,50 @@
+//
+//  HomeViewController.swift
+//  MatStar
+//
+//  Created by uiskim on 2022/10/12.
+//  Copyright (c) 2022 Try-ing. All rights reserved.
+//
+
+import Combine
+import UIKit
+
+import CancelBag
+import SnapKit
+
+final class HomeViewController: BaseViewController {
+    var viewModel: HomeViewModel?
+    
+    /// View Model과 bind 합니다.
+    private func bind() {
+        // input
+        
+        // output
+        
+    }
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUI()
+        bind()
+    }
+}
+
+// MARK: - UI
+extension HomeViewController {
+    private func setUI() {
+        setAttributes()
+        setConstraints()
+    }
+    
+    /// Attributes를 설정합니다.
+    private func setAttributes() {
+        
+    }
+    
+    /// 화면에 그려질 View들을 추가하고 SnapKit을 사용하여 Constraints를 설정합니다.
+    private func setConstraints() {
+        
+    }
+}
---
+++ Sources/Presenter/Home/HomeViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewModel.swift
+//  MatStar
+//
+//  Created by uiskim on 2022/10/12.
+//  Copyright (c) 2022 Try-ing. All rights reserved.
+//
+
+import Combine
+
+final class HomeViewModel: BaseViewModel {
+
+}
