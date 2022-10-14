5598fdfb9d52498f6d301ae29331784812ce7d84
Charlie <tmdckd232@naver.com>
2022-10-14 17:13:08 +0900

    ✨DesignSystem Color 추가

---
+++ Sources/Utility/Extensions/UIColor+.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor+.swift
+//  MatStar
+//
+//  Created by 김승창 on 2022/10/14.
+//  Copyright © 2022 Try-ing. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    class func designSystem(_ color: Palette) -> UIColor? {
+        UIColor(named: color.rawValue)
+//        UIColor(named: color.rawValue, in: Bundle.module, compatibleWith: nil)
+    }
+}
