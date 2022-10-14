cee6a69079692168cf9a15b6bf7de5ebcab60ce4
wody-d <maxwodyd98@gmail.com>
2022-10-14 14:06:54 +0900

    🎉 Login Scene 생성

---
+++ Sources/Presenter/Intro/Login/LoginViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LoginViewController.swift
+//  MatStar
+//
+//  Created by Jaeyong Lee on 2022/10/14.
+//  Copyright (c) 2022 Try-ing. All rights reserved.
+//
+
+import Combine
+import UIKit
+
+import CancelBag
+import SnapKit
+import PinLayout
+import FlexLayout
+
+final class LoginViewController: BaseViewController {
+    var viewModel: LoginViewModel?
+    
+    private func bind() {
+
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUI()
+        bind()
+    }
+}
+
+// MARK: - UI
+extension LoginViewController {
+    private func setUI() {
+
+    }
+}
