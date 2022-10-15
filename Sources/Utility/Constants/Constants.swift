c64df13fec054f183c6b58802b5eb6961ab0fd56
Charlie <tmdckd232@naver.com>
2022-10-15 19:16:39 +0900

    ✨Constants 추가 (Screen, Image)

--- Sources/Utility/Constants/Constants.swift
+++ Sources/Utility/Constants/Constants.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2022 Try-ing. All rights reserved.
 //
 
+import UIKit
+
 struct Constants {
     // Coordinator에서 사용하는 문자열 상수들입니다.
     struct Coordinator {
@@ -18,5 +20,15 @@ struct Constants {
         static let searchIcon = "ic_search"
         static let feedIcon = "ic_feed"
         static let profileIcon = "ic_profile"
+    }
+    
+    struct Screen {
+        static let width = UIScreen.main.bounds.width
+        static let height = UIScreen.main.bounds.height
+    }
+    
+    struct Image {
+        static let chevron_left = "chevron_left"
+        static let x_mark = "x_mark"
     }
 }
