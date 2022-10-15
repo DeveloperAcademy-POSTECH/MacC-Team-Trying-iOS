5046c289d8f302d48dd8fa01c419c6586b68bb9b
Charlie <tmdckd232@naver.com>
2022-10-16 01:17:11 +0900

    💡DocC 주석 추가

--- Sources/Utility/Extensions/UIButton+.swift
+++ Sources/Utility/Extensions/UIButton+.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 extension UIButton {
+    /// 버튼 상태에 따른 배경 색상을 설정합니다.
+    /// - Parameters:
+    ///   - color: 적용할 색상
+    ///   - state: 버튼의 상태
     func setBackgroundColor(_ color: UIColor, for state: UIControl.State) {
         UIGraphicsBeginImageContext(CGSize(width: 1.0, height: 1.0))
         guard let context = UIGraphicsGetCurrentContext() else { return }
--- Sources/Utility/Extensions/UIView+.swift
+++ Sources/Utility/Extensions/UIView+.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 extension UIView {
+    /// 화면에 여러 View들을 추가합니다
+    /// - Parameter views: 추가할 View들
     func addSubviews(_ views: UIView...) {
         views.forEach { view in
             addSubview(view)
