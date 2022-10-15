3f6808994c92c173639fa7e325aa353f008939b5
Charlie <tmdckd232@naver.com>
2022-10-15 19:14:27 +0900

    ✨Asset 추가 (아이콘, color, font)

--- Sources/DesignSystem/Color/Palette.swift
+++ Sources/DesignSystem/Color/Palette.swift
@@ -9,14 +9,28 @@
 enum Palette: String {
     case mainYellow
     case grayC5C5C5
+    case grayEBEBF5
+    case gray767680
+    case black
+    case red
+    case white
 
     var hexString: String {
         switch self {
         case .mainYellow:
             return "#FFF56AFF"
-            
         case .grayC5C5C5:
             return "#C5C5C5FF"
+        case .grayEBEBF5:
+            return "#EBEBF599"
+        case .gray767680:
+            return "#7676803D"
+        case .black:
+            return "#000000FF"
+        case .red:
+            return "#FF0000FF"
+        case .white:
+            return "#FFFFFFFF"
         }
     }
 }
