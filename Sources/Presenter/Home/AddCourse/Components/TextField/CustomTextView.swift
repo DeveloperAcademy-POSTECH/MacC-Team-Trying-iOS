153981c918537d8a798230c28018a8c175a9fc4a
Charlie <tmdckd232@naver.com>
2022-10-15 19:17:13 +0900

    ✨TextField, TextView 구현

---
+++ Sources/Presenter/Home/AddCourse/Components/TextField/CustomTextView.swift
@@ -0,0 +1,58 @@
+//
+//  CustomTextView.swift
+//  MatStar
+//
+//  Created by 김승창 on 2022/10/15.
+//  Copyright © 2022 Try-ing. All rights reserved.
+//
+
+import UIKit
+
+import SnapKit
+
+final class CustomTextView: UITextView {
+    init() {
+        super.init(frame: .zero, textContainer: nil)
+        
+        setAttributes()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setAttributes() {
+        self.autocorrectionType = .no
+        self.textContainerInset = UIEdgeInsets(top: 15, left: 10, bottom: 10, right: 10)
+        self.layer.cornerRadius = 15
+        self.textColor = .designSystem(.white)
+        self.backgroundColor = .designSystem(.gray767680)
+        
+        self.tintColor = .designSystem(.white)
+        self.text = "내용을 입력해 주세요."
+        self.font = .designSystem(weight: .regular, size: ._15)
+        self.delegate = self
+    }
+    
+    private func setLayout() {
+        self.snp.makeConstraints { make in
+            make.width.equalTo(Constants.Screen.width * 0.8974)
+            make.height.equalTo(Constants.Screen.height * 0.4383)
+        }
+    }
+}
+
+extension CustomTextView: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.text == "내용을 입력해 주세요." {
+            textView.text = nil
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = "내용을 입력해 주세요."
+        }
+    }
+}
---
+++ Sources/Utility/Extensions/UITextField+.swift
@@ -0,0 +1,27 @@
+//
+//  UITextField+.swift
+//  MatStar
+//
+//  Created by 김승창 on 2022/10/15.
+//  Copyright © 2022 Try-ing. All rights reserved.
+//
+
+import UIKit
+
+extension UITextField {
+    /// UITextField의 leftView image를 설정합니다.
+    /// - Parameters:
+    ///   - image: leftView에 들어갈 UIImage
+    ///   - imageWidth: image width
+    ///   - padding: ImageView에 적용할 padding
+    func leftImage(_ image: UIImage?, imageWidth: CGFloat, padding: CGFloat) {
+        let imageView = UIImageView(image: image)
+        imageView.frame = CGRect(x: padding, y: 0, width: imageWidth, height: frame.height)
+        imageView.contentMode = .center
+        
+        let containerView = UIView(frame: CGRect(x: 0, y: 0, width: imageWidth + 2 * padding, height: frame.height))
+        containerView.addSubview(imageView)
+        leftView = containerView
+        leftViewMode = .always
+    }
+}
