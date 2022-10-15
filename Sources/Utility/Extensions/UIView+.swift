fcf062c0f7a0708513681dd9e266d87fe7cd6cc1
Charlie <tmdckd232@naver.com>
2022-10-15 19:18:37 +0900

    ♻️TestViewController, StatusBarStyle 수정

---
+++ Sources/Utility/Extensions/UIView+.swift
@@ -0,0 +1,17 @@
+//
+//  UIView+.swift
+//  MatStar
+//
+//  Created by 김승창 on 2022/10/14.
+//  Copyright © 2022 Try-ing. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func addSubviews(_ views: UIView...) {
+        views.forEach { view in
+            addSubview(view)
+        }
+    }
+}
