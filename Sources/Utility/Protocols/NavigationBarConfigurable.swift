e4edb889da503f4887a833c7eb18b1391cd4696d
Charlie <tmdckd232@naver.com>
2022-10-17 22:40:58 +0900

    ✨NavigationBarConfigurable Protocol 구현

---
+++ Sources/Utility/Protocols/NavigationBarConfigurable.swift
@@ -0,0 +1,72 @@
+//
+//  NavigationBarConfigurable.swift
+//  MatStar
+//
+//  Created by 김승창 on 2022/10/17.
+//  Copyright © 2022 Try-ing. All rights reserved.
+//
+
+import UIKit
+
+protocol NavigationBarConfigurable: BaseViewController {
+    /// 코스 등록 과정에서 지도가 있는 화면에서 사용되는 Navigation Bar를 설정합니다.
+    /// - Parameters:
+    ///   - target: Target
+    ///   - action: Back Button이 눌렸을 때 실행할 @objc 메소드
+    func configureMapNavigationBar(target: Any?, action: Selector?)
+    
+    /// 코스 등록 과정에서 장소를 검색할 수 있는 화면에서 사용되는 Navigation Bar를 설정합니다.
+    /// - Parameters:
+    ///   - target: Target
+    ///   - action: Back Button이 눌렸을 때 실행할 @objc 메소드
+    func configureSearchNavigationBar(target: Any?, action: Selector?)
+    
+    /// 코스 등록 과정에서 코스 이름, 내용, 사진을 입력하는 화면에서 사용되는 Navigation Bar를 설정합니다.
+    /// - Parameters:
+    ///   - target: Target
+    ///   - action: Back Button이 눌렸을 때 실행할 @objc 메소드
+    func configureCourseDetailNavigationBar(target: Any?, action: Selector?)
+}
+
+extension NavigationBarConfigurable {
+    func configureMapNavigationBar(target: Any?, action: Selector?) {
+        let backButton = UIBarButtonItem(
+            image: UIImage(named: Constants.Image.x_mark)?
+                .withAlignmentRectInsets(UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 20)),
+            style: .plain,
+            target: target,
+            action: action
+        )
+        let titleView = CustomTextField(type: .search)
+        
+        navigationItem.leftBarButtonItem = backButton
+        navigationItem.titleView = titleView
+    }
+    
+    func configureSearchNavigationBar(target: Any?, action: Selector?) {
+        let backButton = UIBarButtonItem(
+            image: UIImage(named: Constants.Image.chevron_left)?
+                .withAlignmentRectInsets(UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 34)),
+            style: .plain,
+            target: target,
+            action: action
+        )
+        let titleView = CustomTextField(type: .search)
+        
+        navigationItem.leftBarButtonItem = backButton
+        navigationItem.titleView = titleView
+    }
+    
+    func configureCourseDetailNavigationBar(target: Any?, action: Selector?) {
+        let backButton = UIBarButtonItem(
+            image: UIImage(named: Constants.Image.chevron_left),
+            style: .plain,
+            target: target,
+            action: action
+        )
+        let titleView = SmallRoundButton(type: .selectDate)
+        
+        navigationItem.leftBarButtonItem = backButton
+        navigationItem.titleView = titleView
+    }
+}
