e75dbb15d9b15f2f0e70afd2f2a3e7b2788036bf
wody-d <maxwodyd98@gmail.com>
2022-10-24 04:23:42 +0900

    🎉인증코드 입력 Scene 생성

---
+++ Sources/Presenter/Intro/EnterCode/EnterCodeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  EnterCodeViewController.swift
+//  MatStar
+//
+//  Created by Jaeyong Lee on 2022/10/24.
+//  Copyright (c) 2022 Try-ing. All rights reserved.
+//
+
+import Combine
+import UIKit
+
+import CancelBag
+import SnapKit
+
+final class EnterCodeViewController: IntroBaseViewController<EnterCodeViewModel> {
+
+}
+
+extension EnterCodeViewController {
+    
+}
