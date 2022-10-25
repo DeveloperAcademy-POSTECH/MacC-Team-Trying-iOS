1021fb86814f7def0c187eb98b54699824b684e5
kimscastle <uisong26559287@gmail.com>
2022-10-25 17:52:47 +0900

    [수정] 에러처리 수정

--- Sources/Presenter/Home/MyHome/HomeAPIService.swift
+++ Sources/Presenter/Home/MyHome/HomeAPIService.swift
@@ -8,21 +8,27 @@
 
 import Foundation
 
+enum HomeApiError: Error {
+    case urlResponse
+    case response
+}
+
 let fetchUserUrl = "http://15.165.72.196:3059/users/me"
 let token = "Bearer eyJhbGciOiJIUzUxMiJ9.eyJpZCI6IjdkNTk4YWFmLTAwNmQtNDZlYy05ODZiLWFjMmQzN2U1NjdkYiIsImF1dGgiOiJVU0VSIn0.Pa8oQyIVeua1HzIGuBIYwFU-TsHV7t6POhrZrI84TRHyzCpw-ELKTbejqdFSg1Jknzt8snDZhV10-MjyuKR_jw"
 
 class HomeAPIService {
+    
     static func fetchUserAsync() async throws -> Data {
-        var request = URLRequest(url: URL(string: fetchUserUrl)!)
-        request.setValue(token, forHTTPHeaderField: "accessToken")
-        do {
-            let (data, response) = try await URLSession.shared.data(for: request)
-            dump(response)
-            return data
-        } catch {
-            print("오류")
-            fatalError()
+        guard let url = URL(string: fetchUserUrl) else {
+            throw HomeApiError.urlResponse
         }
+        var request = URLRequest(url: url)
+        request.setValue(token, forHTTPHeaderField: "accessToken")
+        let (data, response) = try await URLSession.shared.data(for: request)
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw HomeApiError.response
+        }
+        return data
     }
     
 //    static func fetchUser(onComplete: @escaping (Result<Data, Error>) -> Void) {
