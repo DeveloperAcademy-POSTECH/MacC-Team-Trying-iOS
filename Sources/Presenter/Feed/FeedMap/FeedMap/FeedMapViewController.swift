0e138ada529427c76927cc4e29da176a45a93ece
luminoux <yungzin98@naver.com>
2022-10-25 14:28:59 +0900

    [기능] Feed -> FeedMap 화면 전환

--- Sources/Presenter/Feed/FeedMap/FeedMap/FeedMapViewController.swift
+++ Sources/Presenter/Feed/FeedMap/FeedMap/FeedMapViewController.swift
@@ -11,10 +11,11 @@ import UIKit
 
 import CancelBag
 import SnapKit
+import MapKit
 
 final class FeedMapViewController: BaseViewController {
     var viewModel: FeedMapViewModel?
-    
+    private var mapView = MKMapView()
     /// View Model과 bind 합니다.
     private func bind() {
         // input
@@ -25,22 +26,44 @@ final class FeedMapViewController: BaseViewController {
 
     public override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        view.addSubviews(mapView)
+
+        let location = CLLocationCoordinate2D(latitude: 37.5030042, longitude: 127.0507571)
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let region = MKCoordinateRegion(center: location, span: span)
+
+        mapView.setRegion(region, animated: true)
+
+        let annotation = MKPointAnnotation()
+        annotation.title = "HERE!!!!"
+        annotation.coordinate = location
+        mapView.addAnnotation(annotation)
+
+        mapView.delegate = self
+        mapView.snp.makeConstraints { make in
+            make.center.width.height.equalToSuperview()
+        }
         setUI()
         bind()
     }
 }
 
+extension FeedMapViewController: MKMapViewDelegate {
+
+}
+
 // MARK: - UI
-extension FeedMapViewController {
+extension FeedMapViewController: NavigationBarConfigurable{
     private func setUI() {
+        configureMapNavigationBar(target: self, dismissAction: #selector(backButtonPressed(_:)), pushAction: #selector(backButtonPressed(_:)))
         setAttributes()
         setConstraints()
     }
     
     /// Attributes를 설정합니다.
     private func setAttributes() {
-        
+        navigationController?.tabBarController?.tabBar.isHidden = true
     }
     
     /// 화면에 그려질 View들을 추가하고 SnapKit을 사용하여 Constraints를 설정합니다.
@@ -49,3 +72,40 @@ extension FeedMapViewController {
     }
 }
 
+extension FeedMapViewController {
+
+    @objc
+    func backButtonPressed(_ sender: UIButton) {
+        print("backButton Pressed")
+    }
+
+
+}
+
+#if DEBUG
+
+import SwiftUI
+struct FeedMapViewControllerReperesentable: UIViewControllerRepresentable {
+
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        // empty
+    }
+
+    @available(iOS 13.0.0, *)
+    func makeUIViewController(context: Context) -> UIViewController {
+        FeedMapViewController()
+    }
+}
+
+@available(iOS 13.0.0, *)
+struct FeedMapViewControllerRepresentablePreviewProvider: PreviewProvider {
+    static var previews: some View {
+        Group {
+            FeedMapViewControllerReperesentable()
+                .ignoresSafeArea()
+                .previewDisplayName("Preview")
+                .previewDevice(PreviewDevice(rawValue: "iPhone 13"))
+        }
+    }
+}
+#endif
