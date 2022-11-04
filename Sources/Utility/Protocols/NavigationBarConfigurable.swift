931bda5ce2d89910d7b4b847cef993dda4324aec
Charlie <tmdckd232@naver.com>
2022-11-04 17:34:01 +0900

    ♻️Navigation Bar 수정

--- Sources/Utility/Protocols/NavigationBarConfigurable.swift
+++ Sources/Utility/Protocols/NavigationBarConfigurable.swift
@@ -11,12 +11,19 @@ import UIKit
 import SnapKit
 
 protocol NavigationBarConfigurable: BaseViewController {
-    /// 코스 등록 과정에서 지도가 있는 화면에서 사용되는 Navigation Bar를 설정합니다.
+    /// 데이트 기록 과정에서 지도가 있는 화면에서 사용되는 Navigation Bar를 설정합니다.
     /// - Parameters:
     ///   - target: Target
     ///   - dismissAction: Back Button이 눌렸을 때 실행할 @objc 메소드
     ///   - pushAction: Next Button이 눌렸을 때 실행할 @objc 메소드
-    func configureMapNavigationBar(target: Any, dismissAction: Selector, pushAction: Selector)
+    func configureRecordMapNavigationBar(target: Any, dismissAction: Selector, pushAction: Selector)
+    
+    /// 데이트 계획 과정에서 지도가 있는 화면에서 사용되는 Navigation Bar를 설정합니다.
+    /// - Parameters:
+    ///   - target: Target
+    ///   - dismissAction: Back Button이 눌렸을 때 실행할 @objc 메소드
+    ///   - pushAction: Next Button이 눌렸을 때 실행할 @objc 메소드
+    func configurePlanMapNavigationNar(target: Any, dismissAction: Selector, pushAction: Selector)
     
     /// 코스 등록 과정에서 장소를 검색할 수 있는 화면에서 사용되는 Navigation Bar를 설정합니다.
     /// - Parameters:
@@ -26,16 +33,15 @@ protocol NavigationBarConfigurable: BaseViewController {
     ///   - textEditingAction: TextField 편집 시 실행할 @objc 메소드
     func configureSearchNavigationBar(target: Any, popAction: Selector, doneAction: Selector, textEditingAction: Selector)
     
-    /// 코스 등록 과정에서 코스 이름, 내용, 사진을 입력하는 화면에서 사용되는 Navigation Bar를 설정합니다.
+    /// 데이트 기록 과정에서 사진, 데이트 후기를 입력하는 화면에서 사용되는 Navigation Bar를 설정합니다.
     /// - Parameters:
     ///   - target: Target
     ///   - popAction: Back Button이 눌렸을 때 실행할 @objc 메소드
-    ///   - selectDateAction: 일정 선택 버튼이 눌렸을 때 실행할 @objc 메소드
-    func configureCourseDetailNavigationBar(target: Any, popAction: Selector, selectDateAction: Selector)
+    func configureCourseDetailNavigationBar(target: Any, popAction: Selector)
 }
 
 extension NavigationBarConfigurable {
-    func configureMapNavigationBar(target: Any, dismissAction: Selector, pushAction: Selector) {
+    func configureRecordMapNavigationBar(target: Any, dismissAction: Selector, pushAction: Selector) {
         let dismissButton: UIButton = {
             let button = UIButton()
             button.setImage(UIImage(named: Constants.Image.navBarDeleteButton), for: .normal)
@@ -44,14 +50,60 @@ extension NavigationBarConfigurable {
         }()
         
         let nextButton: UIButton = {
-            let button = UIButton(type: .custom)
-            button.setTitle("어디를 방문하셨나요?", for: .normal)
-            button.contentHorizontalAlignment = .left
-            button.contentEdgeInsets = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 0)
-            button.setTitleColor(.designSystem(.grayC5C5C5), for: .normal)
-            button.backgroundColor = .designSystem(.black)
-            button.titleLabel?.font = .designSystem(weight: .regular, size: ._15)
-            button.layer.cornerRadius = 22
+            var configuration = UIButton.Configuration.filled()
+            var attributedString = AttributedString.init("어디를 방문하셨나요?")
+            attributedString.font = .designSystem(weight: .regular, size: ._15)
+            configuration.attributedTitle = attributedString
+            configuration.titleAlignment = .leading
+            configuration.contentInsets = .init(top: 0, leading: 0, bottom: 0, trailing: 110)
+            let symbolConfiguration = UIImage.SymbolConfiguration(pointSize: 15, weight: .bold, scale: .medium)
+            let image = UIImage(systemName: Constants.Image.magnifyingglass, withConfiguration: symbolConfiguration)
+            configuration.image = image
+            configuration.imagePlacement = .leading
+            configuration.imagePadding = 5
+            configuration.cornerStyle = .capsule
+            configuration.baseBackgroundColor = .designSystem(.black)
+            configuration.baseForegroundColor = .designSystem(.grayC5C5C5)
+            let button = UIButton(configuration: configuration)
+            button.addTarget(target, action: pushAction, for: .touchUpInside)
+            return button
+        }()
+        
+        nextButton.snp.makeConstraints { make in
+            make.width.equalTo(DeviceInfo.screenWidth * 0.77)
+            make.height.equalTo(44)
+        }
+        
+        let leftButtonItem = UIBarButtonItem(customView: dismissButton)
+        let rightButtonItem = UIBarButtonItem(customView: nextButton)
+        navigationItem.leftBarButtonItem = leftButtonItem
+        navigationItem.rightBarButtonItem = rightButtonItem
+    }
+    
+    func configurePlanMapNavigationNar(target: Any, dismissAction: Selector, pushAction: Selector) {
+        let dismissButton: UIButton = {
+            let button = UIButton()
+            button.setImage(UIImage(named: Constants.Image.navBarDeleteButton), for: .normal)
+            button.addTarget(target, action: dismissAction, for: .touchUpInside)
+            return button
+        }()
+        
+        let nextButton: UIButton = {
+            var configuration = UIButton.Configuration.filled()
+            var attributedString = AttributedString.init("어디를 방문하실 계획인가요??")
+            attributedString.font = .designSystem(weight: .regular, size: ._15)
+            configuration.attributedTitle = attributedString
+            configuration.titleAlignment = .leading
+            configuration.contentInsets = .init(top: 0, leading: 0, bottom: 0, trailing: 110)
+            let symbolConfiguration = UIImage.SymbolConfiguration(pointSize: 15, weight: .bold, scale: .medium)
+            let image = UIImage(systemName: Constants.Image.magnifyingglass, withConfiguration: symbolConfiguration)
+            configuration.image = image
+            configuration.imagePlacement = .leading
+            configuration.imagePadding = 5
+            configuration.cornerStyle = .capsule
+            configuration.baseBackgroundColor = .designSystem(.black)
+            configuration.baseForegroundColor = .designSystem(.grayC5C5C5)
+            let button = UIButton(configuration: configuration)
             button.addTarget(target, action: pushAction, for: .touchUpInside)
             return button
         }()
@@ -103,21 +155,15 @@ extension NavigationBarConfigurable {
         navigationItem.rightBarButtonItem = rightButtonItem
     }
     
-    func configureCourseDetailNavigationBar(target: Any, popAction: Selector, selectDateAction: Selector) {
+    func configureCourseDetailNavigationBar(target: Any, popAction: Selector) {
         let backButton: UIButton = {
             let button = UIButton(type: .custom)
             button.setImage(UIImage(named: Constants.Image.chevron_left), for: .normal)
             button.addTarget(target, action: popAction, for: .touchUpInside)
             return button
         }()
-        let selectDateButton: SmallRoundButton = {
-            let button = SmallRoundButton(type: .selectDate)
-            button.addTarget(target, action: selectDateAction, for: .touchUpInside)
-            return button
-        }()
         
         let leftButtonItem = UIBarButtonItem(customView: backButton)
         navigationItem.leftBarButtonItem = leftButtonItem
-        navigationItem.titleView = selectDateButton
     }
 }
