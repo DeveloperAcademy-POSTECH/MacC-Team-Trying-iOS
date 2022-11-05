510423caa83f3431f41bbbabfef3f27e73910938
Charlie <tmdckd232@naver.com>
2022-11-05 15:11:29 +0900

    ✨HapticManager 구현

--- Sources/Utility/HapticManager/HapticManager.swift
+++ Sources/Utility/HapticManager/HapticManager.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+import UIKit
+
+final class HapticManager {
+    static let instance = HapticManager()
+    
+    private init() {}
+    
+    func notification(type: UINotificationFeedbackGenerator.FeedbackType) {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(type)
+    }
+    
+    func impact(style: UIImpactFeedbackGenerator.FeedbackStyle) {
+        let generator = UIImpactFeedbackGenerator(style: style)
+        generator.impactOccurred()
+    }
+    
+    func selection() {
+        let generator = UISelectionFeedbackGenerator()
+        generator.selectionChanged()
+    }
+}
