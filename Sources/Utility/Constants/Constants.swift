63ac19feef45cc5a950c6b09c0998b2baeae0062
Charlie <tmdckd232@naver.com>
2022-11-09 15:24:22 +0900

    ♻️코스등록 PlaceSearch empty result 시 이미지 변경

--- Sources/Utility/Constants/Constants.swift
+++ Sources/Utility/Constants/Constants.swift
@@ -39,6 +39,7 @@ struct Constants {
         static let pencil = "pencil"
         static let map = "map"
         static let setting = "gearshape.fill"
+        static let emptyResultStar = "empty_result_star"
     }
     
     struct Lottie {
