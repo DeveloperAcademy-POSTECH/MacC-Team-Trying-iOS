502ea0dda81819aa11c04a7b03f86f20b3be2a5d
Charlie <tmdckd232@naver.com>
2022-11-11 18:33:07 +0900

    ✨PlaceListView 중간 높이 추가 (BottomHidable 프로토콜 생성 및 코드 개선)
    PlaceDetailView, PlaceListView 코드 개선

---
+++ Sources/Utility/Protocols/BottomHidable.swift
@@ -0,0 +1,15 @@
+//
+//  BottomHidable.swift
+//  ComeIt
+//
+//  Created by 김승창 on 2022/11/11.
+//  Copyright © 2022 Try-ing. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol BottomHidable: UIView {
+    func hide()
+    func present()
+}
