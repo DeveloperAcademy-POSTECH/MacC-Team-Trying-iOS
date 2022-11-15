e17718cd269d49b958e3d276c5eede43ad625ed6
Charlie <tmdckd232@naver.com>
2022-11-15 09:33:30 +0900

    ✨AppDelegate APNs 처리

--- Sources/App/AppDelegate.swift
+++ Sources/App/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var coordinator: AppCoordinator?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        self.registerForRemoteNotifications()
+        
         KakaoSDK.initSDK(appKey: "041c741d45744f54da6ed10e0f946672")
         self.window = UIWindow(frame: UIScreen.main.bounds)
 
@@ -33,5 +35,40 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         return false
     }
-
 }
+
+// MARK: - Push Notifications
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    private func registerForRemoteNotifications() {
+        let notificationCenter = UNUserNotificationCenter.current()
+        notificationCenter.delegate = self
+        let options: UNAuthorizationOptions = [.alert, .sound, .badge]
+        notificationCenter.requestAuthorization(options: options) { granted, error in
+            guard granted else { return }
+            
+            DispatchQueue.main.async {
+                UIApplication.shared.registerForRemoteNotifications()
+            }
+        }
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        // 앱이 foreground 상태일 때 push가 온 경우
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse) async {
+        // push를 탭한 경우
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        // 디바이스 토큰 등록 성공 시
+        let deviceTokenString = deviceToken.map { String(format: "%02x", $0) }.joined()
+        print(deviceTokenString)
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        // 디바이스 토큰 등록 실패 시
+        print("Failed to register for notifications: \(error.localizedDescription)")
+    }
+}
+
