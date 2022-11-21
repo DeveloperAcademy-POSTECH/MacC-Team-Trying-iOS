371bd3e42e5fd9f3d8a06e0f3e5cb0eb2c8d2b70
luminoux <yungzin98@naver.com>
2022-11-22 05:10:43 +0900

    [기능] UIImageView URL Load extension 추가

--- Sources/Utility/Extensions/UIImageView+.swift
+++ Sources/Utility/Extensions/UIImageView+.swift
@@ -16,4 +16,16 @@ extension UIImageView {
         blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         addSubview(blurEffectView)
     }
+    
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
 }
