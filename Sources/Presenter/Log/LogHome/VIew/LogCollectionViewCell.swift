666f062c5e59410c03a639872eaacd81d3f6e555
luminoux <yungzin98@naver.com>
2022-11-21 21:53:02 +0900

    [UI] 별자리 이미지 수정

--- Sources/Presenter/Log/LogHome/VIew/LogCollectionViewCell.swift
+++ Sources/Presenter/Log/LogHome/VIew/LogCollectionViewCell.swift
@@ -61,7 +61,7 @@ extension LogCollectionViewCell {
             }
             .forEach { $0.removeFromSuperview() }
         
-        let courseView = makeConstellation(places: course.places)
+        let courseView = StarMaker.makeConstellation(places: course.places)
         
         contentView.addSubview(courseView)
         
@@ -99,63 +99,5 @@ extension LogCollectionViewCell {
             make.centerX.equalToSuperview()
             make.bottom.equalTo(dateLabel.snp.top).offset(-10)
         }
-    }
-    
-    func makeConstellation(places: [PlaceEntity]) -> UIView {
-        let constellationView = UIView()
-        constellationView.backgroundColor = .clear
-        constellationView.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
-        
-        let latitudeArray = places.map { CGFloat($0.coordinate.latitude) }
-        let longitudeArray = places.map { CGFloat($0.coordinate.longitude) }
-        
-        guard let minX = latitudeArray.min(),
-              let maxX = latitudeArray.max(),
-              let minY = longitudeArray.min(),
-              let maxY = longitudeArray.max() else { return UIView() }
-        
-        let deltaX: CGFloat = maxX == minX ? 1 : maxX - minX
-        let deltaY: CGFloat = maxY == minY ? 1 : maxY - minY
-        
-        let adjustedLatitude = latitudeArray.map { (CGFloat(($0 - minX) / deltaX ) * 200) * 0.8 }
-        let adjustedLongitude = longitudeArray.map { (CGFloat(($0 - minY) / deltaY) * 200) * 0.8 }
-        
-        let xOffset = (200 - abs(adjustedLatitude.max()!)) / 2 - 12.5
-        let yOffset = (200 - abs(adjustedLongitude.max()!)) / 2 - 12.5
-        
-        for index in places.indices {
-            let randomStarLottieSize = CGFloat.random(in: (30.0...60.0))
-            let starLottie = LottieAnimationView(name: Constants.Lottie.mainStar)
-            starLottie.contentMode = .scaleAspectFit
-            starLottie.frame = CGRect(x: adjustedLatitude[index] + xOffset, y: adjustedLongitude[index] + yOffset, width: randomStarLottieSize, height: randomStarLottieSize)
-            starLottie.animationSpeed = CGFloat.random(in: 0.05...0.3)
-            starLottie.animationSpeed = 0.6
-            starLottie.loopMode = .loop
-            starLottie.play(fromProgress: 0.0, toProgress: 0.9935)
-            
-            constellationView.addSubview(starLottie)
-            
-            if index < places.count - 1 {
-                let xPan = (adjustedLatitude[index + 1] - adjustedLatitude[index])
-                let yPan = (adjustedLongitude[index + 1] - adjustedLongitude[index])
-                
-                let distance = ((xPan * xPan) + (yPan * yPan)).squareRoot()
-                let editX = 13 / distance * xPan
-                let editY = 13 / distance * yPan
-                
-                let path = UIBezierPath()
-                path.move(to: CGPoint(x: adjustedLatitude[index] + starLottie.frame.size.width / 2 + editX + xOffset, y: adjustedLongitude[index] + starLottie.frame.size.height / 2 + editY + yOffset))
-                path.addLine(to: CGPoint(x: adjustedLatitude[index + 1] + starLottie.frame.size.width / 2 - editX + xOffset, y: adjustedLongitude[index + 1] + starLottie.frame.size.height / 2 - editY + yOffset))
-                path.lineWidth = 0.15
-                path.close()
-                
-                let shapeLayer = CAShapeLayer()
-                shapeLayer.path = path.cgPath
-                shapeLayer.lineWidth = path.lineWidth
-                shapeLayer.strokeColor = .designSystem(.whiteFFFBD9)
-                constellationView.layer.addSublayer(shapeLayer)
-            }
-        }
-        return constellationView
     }
 }
