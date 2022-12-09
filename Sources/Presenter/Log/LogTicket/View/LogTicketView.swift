b69925549d2c4492390650ece0b826aa767b31e8
luminoux <yungzin98@naver.com>
2022-12-09 19:53:14 +0900

    :bug: LogTicketView -> dismissButton 안 눌리는 에러 해결

--- Sources/Presenter/Log/LogTicket/View/LogTicketView.swift
+++ Sources/Presenter/Log/LogTicket/View/LogTicketView.swift
@@ -16,12 +16,7 @@ class LogTicketView: UIView {
     
     private let viewModel: LogTicketViewModel
     
-    var imageUrl: [String] = [] {
-        didSet {
-            setCollectionView()
-            setPageControl()
-        }
-    }
+    var imageUrl: [String] = []
     
     lazy var blurEffectView: UIVisualEffectView = {
         let blurEffectView = UIVisualEffectView(
@@ -112,8 +107,9 @@ class LogTicketView: UIView {
     }()
     
     // MARK: Initializer
-    init(viewModel: LogTicketViewModel) {
+    init(viewModel: LogTicketViewModel, imageUrl: [String]) {
         self.viewModel = viewModel
+        self.imageUrl = imageUrl
         super.init(frame: .zero)
         
         self.addSubviews(
@@ -133,6 +129,8 @@ class LogTicketView: UIView {
         )
         setLayouts()
         setButtonTarget()
+        setCollectionView()
+        setPageControl()
         
         self.addSubview(blurEffectView)
         self.sendSubviewToBack(blurEffectView)
@@ -182,11 +180,12 @@ extension LogTicketView: UIScrollViewDelegate {
     private func setCollectionView() {
         switch imageUrl.isEmpty {
         case true:
+            emptyImageView.backgroundColor = .red
             addSubview(emptyImageView)
             imageCollectionView.isHidden = true
 
             emptyImageView.snp.makeConstraints { make in
-                make.width.equalToSuperview()
+                make.width.equalToSuperview().multipliedBy(0.6)
                 make.height.equalTo(DeviceInfo.screenHeight * 0.3471563981)
                 make.centerX.equalToSuperview()
                 make.top.equalToSuperview()
